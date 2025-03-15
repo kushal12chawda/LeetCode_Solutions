75f92bde744ba378fa5b9d1078da19c57690eead
kushal12chawda <131882408+kushal12chawda@users.noreply.github.com>
2025-03-15 18:03:48 +0530

    Time: 1366 ms (41%), Space: 0B (100%) - LeetHub

---
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -0,0 +1,15 @@
+/* Write your PL/SQL query statement below */
+WITH ProductToYear AS (
+    SELECT product_id, MIN(year) AS year
+    FROM Sales
+    GROUP BY product_id
+)
+SELECT
+    Sales.product_id,
+    ProductToYear.year AS first_year,
+    Sales.quantity,
+    Sales.price
+FROM Sales
+INNER JOIN ProductToYear
+ON Sales.product_id = ProductToYear.product_id 
+AND Sales.year = ProductToYear.year;
