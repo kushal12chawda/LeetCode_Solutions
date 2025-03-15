ad50bf4f1db4cc93eee53ff1611b540a1c207a39
kushal12chawda <131882408+kushal12chawda@users.noreply.github.com>
2025-03-15 18:20:05 +0530

    Time: 1373 ms (30%), Space: 0B (100%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,11 @@
+/* Write your PL/SQL query statement below */
+WITH LogsNeighbors AS (
+    SELECT 
+        id, num, 
+        LAG(num) OVER (ORDER BY id) AS prev_num,
+        LEAD(num) OVER (ORDER BY id) AS next_num
+    FROM LOGS
+)
+SELECT DISTINCT num AS ConsecutiveNums
+FROM LogsNeighbors
+WHERE num = prev_num AND num = next_num;
